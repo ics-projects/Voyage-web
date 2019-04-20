2da1807aaf5e395227169f69ed8eee10b93b224b
johnngugi <johnngugi777@gmail.com>
2019-04-21 00:35:19 +0300

    Fix styles not rendering on homepage

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,6 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js/')
+   .styles('resources/css/*.css', 'public/css/all.css');
+   // .sass('resources/sass/app.scss', 'public/css');
