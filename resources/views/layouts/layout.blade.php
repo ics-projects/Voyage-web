5fd8ab8957009a48c7457a5ffbd7c0b773fc7b92
johnngugi <johnngugi777@gmail.com>
2019-04-22 23:11:06 +0300

    Fix login template errors

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,137 +1,81 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Voyage Transit</title>
-        
-        <!-- Fonts -->
-        <link rel="dns-prefetch" href="//fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-        <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link rel="stylesheet" href={{ asset("css/linearicons.css") }}>
-        <link rel="stylesheet" href={{ asset("css/font-awesome.min.css") }}>
-        <link rel="stylesheet" href={{ asset("css/bootstrap.css") }}>
-        <link rel="stylesheet" href={{ asset("css/magnific-popup.css") }}>
-        <link rel="stylesheet" href={{ asset("css/jquery-ui.css") }}>
-        <link rel="stylesheet" href={{ asset("css/nice-select.css") }}>
-        <link rel="stylesheet" href={{ asset("css/animate.min.css") }}>
-        <link rel="stylesheet" href={{ asset("css/owl.carousel.css") }}>
-        <link rel="stylesheet" href={{ asset("css/main.css") }}>
-    </head>
-    <body>
-        <div id="app">
-            <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-                <div class="container">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        Voyage Transit
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
-                        </ul>
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                            @endif
-                            @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href=""
-                                        onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="no-js">
+
+<head>
+    @include('includes.head')
+</head>
+
+<body>
+    {{--
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                            Voyage Transit
+                        </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <!-- Left Side Of Navbar -->
+                <ul class="navbar-nav mr-auto">
+                </ul>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                    @endif @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
+                            aria-expanded="false" v-pre>
+                                        {{ Auth::user()->name }} <span class="caret"></span>
                                     </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                            @endguest
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-            <main class="py-4">
-                @yield('content')
-            </main>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endguest
+                </ul>
+            </div>
         </div>
-        <!-- start footer Area -->
-        <footer class="footer-area section-gap">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3  col-md-6 col-sm-6">
-                        <div class="single-footer-widget">
-                            <h6>About Agency...</h6>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6">
-                        <div class="single-footer-widget">
-                            <h6>Navigation Links</h6>
-                            <div class="row">
-                                <div class="col">
-                                    <ul>
-                                        <li><a href="#">Home</a></li>
-                                        <li><a href="#">Feature</a></li>
-                                        <li><a href="#">Services</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3  col-md-6 col-sm-6">
-                        <div class="single-footer-widget">
-                            <div id="mc_embed_signup">
-                                <form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get" class="subscription relative">
-                                    <div class="input-group d-flex flex-row">
-                                        <input name="EMAIL" placeholder="Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email Address '" required="" type="email">
-                                        <button class="btn bb-btn"><span class="lnr lnr-location"></span></button>
-                                    </div>
-                                    <div class="mt-10 info"></div>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3  col-md-6 col-sm-6">
-                        <div class="single-footer-widget mail-chimp">
-                            <h6 class="mb-20">Travel with Us</h6>
-                        </div>
-                    </div>
-                </div>
-                <div class="row footer-bottom d-flex justify-content-between align-items-center">
-                    <p class="col-lg-8 col-sm-12 footer-text m-0">
-                    </footer>
-                    <script src={{ asset("js/vendor/jquery-2.2.4.min.js") }}></script>
-                    <script src={{ asset("js/popper.min.js") }}></script>
-                    <script src={{ asset("js/vendor/bootstrap.min.js") }}></script>
-                    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-                    <script src={{ asset("js/jquery-ui.js") }}></script>
-                    <script src={{ asset("js/easing.min.js") }}></script>
-                    <script src={{ asset("js/hoverIntent.js") }}></script>
-                    <script src={{ asset("js/superfish.min.js") }}></script>
-                    <script src={{ asset("js/jquery.ajaxchimp.min.js") }}></script>
-                    <script src={{ asset("js/jquery.magnific-popup.min.js") }}></script>
-                    <script src={{ asset("js/jquery.nice-select.min.js") }}></script>
-                    <script src={{ asset("js/owl.carousel.min.js") }}></script>
-                    <script src={{ asset("js/mail-script.js") }}></script>
-                    <script src={{ asset("js/main.js") }}></script>
-                    <script src={{ asset("js/app.js") }} defer></script>
-                </body>
-            </html>
+    </nav> --}}
+
+    <div id="main">
+        @yield('content')
+    </div>
+    <!-- start footer Area -->
+    <footer class="footer-area section-gap">
+    @include('includes.footer')
+    </footer>
+
+    <script src={{ asset( "js/vendor/jquery-2.2.4.min.js") }}></script>
+    <script src={{ asset( "js/popper.min.js") }}></script>
+    <script src={{ asset( "js/vendor/bootstrap.min.js") }}></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
+    <script src={{ asset( "js/jquery-ui.js") }}></script>
+    <script src={{ asset( "js/easing.min.js") }}></script>
+    <script src={{ asset( "js/hoverIntent.js") }}></script>
+    <script src={{ asset( "js/superfish.min.js") }}></script>
+    <script src={{ asset( "js/jquery.ajaxchimp.min.js") }}></script>
+    <script src={{ asset( "js/jquery.magnific-popup.min.js") }}></script>
+    <script src={{ asset( "js/jquery.nice-select.min.js") }}></script>
+    <script src={{ asset( "js/owl.carousel.min.js") }}></script>
+    <script src={{ asset( "js/mail-script.js") }}></script>
+    <script src={{ asset( "js/main.js") }}></script>
+    <script src={{ asset( "js/app.js") }} defer></script>
+</body>
+
+</html>
