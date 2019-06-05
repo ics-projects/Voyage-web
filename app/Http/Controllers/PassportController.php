e49de61418986f585b7edd6f1bfbc006ef20fe54
johnngugi <johnngugi777@gmail.com>
2019-06-06 02:04:51 +0300

    Add user registration

--- app/Http/Controllers/PassportController.php
+++ app/Http/Controllers/PassportController.php
@@ -2,10 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class PassportController extends Controller
 {
+    /**
+     * Handles Registration Request
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $user = User::create([
+            'first_name' => $request['first_name'],
+            'last_name' => $request['last_name'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+
+        $token = $user->createToken('TutsForWeb')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
     /**
      * Handles Login Request
      *
