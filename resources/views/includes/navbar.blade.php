01e2ce22463260265f4804b2f484cca3619cb10a
Mathenge Waweru <mathenge2waweru@gmail.com>
2019-06-18 18:50:43 +0300

    Adding user profile page

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -7,13 +7,12 @@
                 </div>
                 <div class="col-lg-6 col-sm-6 col-6 header-top-right">
                     <ul>
-                        @guest
-                        @if (Route::has('register'))
-                        <li><a href={{ url('/login') }}>LOG IN</a></li>
-                        <li><a href={{ url('/register') }}>SIGN UP</a></li>
-                        @endif @else
+                        @if (Auth::guest())
+                        <li><a href={{ route('login') }}>LOG IN</a></li>
+                        <li><a href={{ route('register') }}>SIGN UP</a></li>
+                        @else
                         <li class="nav-item">
-                            <a class="nav-link" href="#">{{ Auth::user()->name }}</a>
+                            <a class="nav-link" href="{{ url('user') }}">{{ Auth::user()->first_name }}</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
@@ -24,7 +23,7 @@
                                 @csrf
                             </form>
                         </li>
-                        @endguest
+                        @endif
                     </ul>
                 </div>
             </div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,49 +1,102 @@
 @extends('layouts.layout')
 
+<style>
+    .nav-item {}
+</style>
+
 @section('content')
+<section class="about-banner relative">
+    <div class="overlay overlay-bg"></div>
+    <div class="container">
+        <div class="row d-flex align-items-center justify-content-center">
+            <div class="about-content col-lg-12">
+                <h1 class="text-white">
+                    @if (Auth::check())
+                    Welcome {{Auth::user()->first_name}}
+                    @endif
+                </h1>
+                <p class="text-white link-nav">This is your profile page<span class="lnr lnr-arrow-right"></span></p>
+            </div>
+        </div>
+    </div>
+</section>
+
 <div class="section-gap">
     <div class="container">
-        <div class="col-lg-4 col-md-6">
-
-            <div class="tab-content" id="bookingForm">
-                <div class="tab-pane fade show active" id="flight" role="tabpanel" aria-labelledby="flight-tab">
-                    <form class="form-wrap">
-                        <div class="row align-items-center">
-                            <li class="nav-item">
-                                <p>Book Your Bus</p>
-                            </li>
+        <ul id="nav-tabs" class="nav nav-tabs" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" id="accounttab" role="tab" href="#accountdetails"
+                    aria-selected="true">Account details</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" id="bookingstab" role="tab" href="#bookingdetails"
+                    aria-selected="false">Bookings</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" id="settingstab" role="tab" href="#settings"
+                    aria-selected="false">Settings</a>
+            </li>
+        </ul>
+        <div class="container tab-content">
+            {{-- Account details --}}
+            <div class="container tab-pane show active" id="accountdetails" role="tabpanel"
+                aria-labelledby="accounttab">
+                <p>Account details</p>
+                <form action="{{ route('login') }}" method="post">
+                    @csrf
+                    <article class="card-body mx-auto" style="max-width: 400px;">
+                        <div class="form-group input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"> <i class="fa fa-user" style="font-size:24px"></i>
+                                </span>
+                            </div>
+                            <input name="last_name"
+                                class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}"
+                                placeholder="Last Name" type="text">
+                            @if ($errors->has('last_name'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('last_name') }}</strong>
+                            </span>
+                            @endif
                         </div>
-                        <select class="form-control" id="departure" name="departure" placeholder="where from "
-                            onfocus="this.placeholder = ''" onblur="this.placeholder = 'from '">
-                            @foreach ($trips as $trip)
-                            <option value="{{ $trip->scheduleID->originID->id }}">{{ $trip->scheduleID->originID->name }}
-                            </option>
-                            @endforeach
-                        </select>
-                        <select class="form-control" id="destination" name="destination" placeholder="where to "
-                            onfocus="this.placeholder = ''" onblur="this.placeholder = ' To '">
-                            @foreach ($trips as $trip)
-                            <option value="{{ $trip->scheduleID->destinationID->id }}">
-                                {{ $trip->scheduleID->destinationID->name }}
-                            </option>
-                            @endforeach
-                        </select>
-                        <input type="date" class="form-control" id="date" name="date" placeholder="Date "
-                            onfocus="this.placeholder = ''" onblur="this.placeholder = 'Date '">
-                        <input type="number" min="1" max="20" class="form-control" name="adults" placeholder="Adults "
-                            onfocus="this.placeholder = ''" onblur="this.placeholder = 'Adults '">
-                        <input type="number" min="1" max="20" class="form-control" name="child" placeholder="Child "
-                            onfocus="this.placeholder = ''" onblur="this.placeholder = 'Child '">
-                        <a href="#" id="search" class="primary-btn text-uppercase">Search Buses</a>
-                    </form>
-                </div>
+
+                        <div class="form-group input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"> <i class="fa fa-user" style="font-size:24px"></i>
+                                </span>
+                            </div>
+                            <input name="last_name"
+                                class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}"
+                                placeholder="Last Name" type="text">
+                            @if ($errors->has('last_name'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('last_name') }}</strong>
+                            </span>
+                            @endif
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+                            </div>
+                        </div>
+
+                </form>
+            </div>
+
+            {{-- Booking tabs --}}
+            <div class="container tab-pane" id="bookingdetails" role="tabpanel" aria-labelledby="bookingstab">
+                <p>Bookings</p>
+
+            </div>
+
+            {{-- Settings --}}
+            <div class="container tab-pane" id="settings" role="tabpanel" aria-labelledby="settingstab">
+                <p>Settings</p>
             </div>
         </div>
     </div>
 </div>
-@endsection
-
-@section('scripts')
-@parent
-<script src={{ asset( "js/home-page.js") }}></script>
 @endsection
